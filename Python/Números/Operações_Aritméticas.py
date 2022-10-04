eaed8a4994bf881afb0699c17cb96c24b881b53f
Igor <igor.torquato.20@gmail.com>
2022-10-04 19:09:34 -0300

    Módulo de Números.

---
+++ Python/Números/Operações_Aritméticas.py
@@ -0,0 +1,20 @@
+numero1=10
+numero2=2
+numero3=5.5
+
+#Adição
+print(numero1 + numero3)
+#Subtração
+print(numero1 - numero2)
+#Multiplicação
+print(numero1 * numero2)
+#Divisão
+print(numero1 / numero2)
+#Parênteses como prioridade
+print(numero3 * (numero1 +numero2))
+#Potência
+print(numero1 ** numero2)
+#Divisão Arredondada
+print(21 // 5) # Imprime 4
+#Resto da divisão
+print(numero1 % numero2)
