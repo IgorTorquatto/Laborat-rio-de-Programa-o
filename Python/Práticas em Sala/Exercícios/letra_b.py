8796d513ef4c1679ff4d93a1874cc8733ecec269
Igor <igor.torquato.20@gmail.com>
2022-10-06 16:37:59 -0300

    Correções

--- Python/Práticas em Sala/Exercícios/letra_b.py
+++ Python/Práticas em Sala/Exercícios/letra_b.py
@@ -1,6 +1,6 @@
 lista = [12, -2, 4, 8, 29, 45, 78, 36, -17, 2, 12, 12, 3, 3, -52]
-maiorValor = lista[0]
+menorValor = lista[0]
 for index in range(0, len(lista)):
-    if(maiorValor > lista[index]):
-     maiorValor = lista[index]
-print(maiorValor)
+    if(menorValor > lista[index]):
+     menorValor = lista[index]
+print(menorValor)
