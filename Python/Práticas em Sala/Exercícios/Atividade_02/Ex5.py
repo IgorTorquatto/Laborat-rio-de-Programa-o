16a48fff0123054110ffa0f886226e3fd724d190
Igor T <igor@gmail.com>
2022-10-13 09:35:09 -0300

    Práticas em Sala -> Ex5.py

--- Python/Práticas em Sala/Exercícios/Atividade_02/Ex5.py
+++ Python/Práticas em Sala/Exercícios/Atividade_02/Ex5.py
@@ -2,7 +2,7 @@ import math
 
 
 def cria_vetor(list: list):
-    for i in range(0,3):
+    for i in range(0,2):
         list.append(int(input(f"Digite um número para a posição {i+1} do vetor: ")))
     return list
 
@@ -11,6 +11,11 @@ def calcula_norma(list: list):
     soma_quadrados=0
     for i in range(0,len(list)):
         soma_quadrados= soma_quadrados + (int(list[i])**2)
+    return math.sqrt(soma_quadrados)
+
+
+#def maior(num1,num2,num3):
+    
     
 
 lista_1=[]
@@ -23,4 +28,16 @@ cria_vetor(lista_2)
 print(f"\n Valores para a lista_3: ")
 cria_vetor(lista_3)
 
+print("\n")
 
+norma_1=calcula_norma(lista_1)
+norma_2=calcula_norma(lista_2)
+norma_3=calcula_norma(lista_3)
+soma_normas= norma_1 + (norma_2+norma_3)
+
+print(f"Norma do vetor_1: {norma_1}")
+print(f"Norma do vetor_2: {norma_2}")
+print(f"Norma do vetor_3: {norma_3}")
+
+
+print(f"Soma das normas de todos os vetores: {soma_normas}")
